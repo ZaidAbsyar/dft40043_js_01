ff729ca32758cc2a71e477b1cc9a9d6a9c25928b
ZaidAbsyar <zaidabsyar43@gmail.com>
2021-11-15 14:49:01 +0800

    java

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function cantum() {
+    let fname = document.getElementById('fname').value;
+    let lname = document.getElementById('lname').value;
+    let fullname = fname + ' ' + lname;
+    document.getElementById('fullname').innerText = fullname;
+}
+
